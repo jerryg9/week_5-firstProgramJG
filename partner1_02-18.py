b54555222425cb6b2a0649016ac5307d2966e348
jerryg9 <captainjerry10@yahoo.com>
2026-02-18 18:13:26 +0000

    2/18/26

---
+++ partner1_02-18.py
@@ -0,0 +1,47 @@
+# 1. Print a welcome message
+print("Hello!")
+
+# 2. Create a variable with your name Jerry
+name="Jerry"
+# 3. Print the name
+print(name)
+print("My name is ", name)
+
+# 4. Create a variable with your age 27
+num=27
+
+# 5. Print the age
+print("I am" , 27 , "years old")
+# 6. Ask the user for their favorite color Blue
+favColor = input("What is your favorite color? ")
+
+
+# 7. Print a sentence using the color
+print("Your favorite color is", favColor)
+
+# 8. Create two number variables
+x=5
+y=7
+
+# 9. Add the numbers
+
+result= x + y 
+
+# 10. Print the result
+print(result)
+# 11. Check if age is greater than 18
+if num >18: 
+    
+
+# 12. Print message if true
+    print("You're older than 18!")
+
+# 13. Otherwise print another message
+else:
+# 14. Print message if false
+    print("You're under 18")
+   
+
+# 15. Print a goodbye message
+print("Goodbye, see you Monday")
+#https://www.w3schools.com/Python/python_output_numbers.asp
